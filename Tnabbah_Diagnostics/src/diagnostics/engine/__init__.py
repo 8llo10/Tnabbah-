1f35b1cb398325f1d6a0e2c586b88f0427798f86
Joory Halabi <s444000428@uqu.edu.sa>
2026-05-13 23:53:39 +0300

    حذفت ال ai لانه  في السيرفر

--- Tnabbah_Diagnostics/src/diagnostics/engine/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Rule engine and PID analysis for tnabbah vehicle diagnostics.
-"""
-from .rules_engine import RulesEngine
-from .pid_analyzer import PIDAnalyzer
-
-__all__ = ["RulesEngine", "PIDAnalyzer"]
